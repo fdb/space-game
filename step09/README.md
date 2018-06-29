5a33c133923536b84500c9119e55f12dc74ea110
Frederik De Bleser <frederik@debleser.be>
2018-06-29 19:35:13 +0200

    Step 9.

---
+++ step09/README.md
@@ -0,0 +1,25 @@
+# Step 9 - Random cooldown
+
+This is not very much code but we need to explain our custom `rand` function. It returns a random value between `min` and `max`.
+
+Internally it uses [`Math.random`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random). That function only returns values between 0.0-1.0.
+
+Here's the code for our custom random function:
+
+```
+function rand(min, max) {
+  if (min === undefined) min = 0;
+  if (max === undefined) max = 1;
+  return min + Math.random() * (max - min);
+}
+```
+
+First it sets the default values for the function if we don't provide them. So if we use it just as `rand()`, it will use a minimum value of 0 and a maximum value of 1.
+
+The core line is the third line of our function, `return min + Math.random() * (max - min);`. This does a couple of things:
+
+- It calculates the *range* of the random values. So for example if min = 25 and max = 100, our range of possible values is 75 (100 - 25).
+- It uses `Math.random()` to generate a value between 0 and 1.
+- We multiply this random value with the range. Since the max is 1, the maximum we can have is the range (75 in our example).
+- We then add the minimum value again to *shift* the value up. So by adding 25 to our value between 0-75, it results in a random value between 25 and 100, which is what we want.
+
---
+++ step09/js/game.js
@@ -0,0 +1,255 @@
+const KEY_CODE_LEFT = 37;
+const KEY_CODE_RIGHT = 39;
+const KEY_CODE_SPACE = 32;
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+const PLAYER_WIDTH = 20;
+const PLAYER_MAX_SPEED = 600.0;
+const LASER_MAX_SPEED = 300.0;
+const LASER_COOLDOWN = 0.5;
+
+const ENEMIES_PER_ROW = 10;
+const ENEMY_HORIZONTAL_PADDING = 80;
+const ENEMY_VERTICAL_PADDING = 70;
+const ENEMY_VERTICAL_SPACING = 80;
+const ENEMY_COOLDOWN = 2.0;
+
+const GAME_STATE = {
+  lastTime: Date.now(),
+  leftPressed: false,
+  rightPressed: false,
+  spacePressed: false,
+  playerX: 0,
+  playerY: 0,
+  playerCooldown: 0,
+  lasers: [],
+  enemies: [],
+  enemiesCooldown: 0,
+  enemyLasers: [],
+};
+
+function rectsIntersect(r1, r2) {
+  return !(
+    r2.left > r1.right ||
+    r2.right < r1.left ||
+    r2.top > r1.bottom ||
+    r2.bottom < r1.top
+  );
+}
+
+function setPosition(el, x, y) {
+  el.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+function clamp(v, min, max) {
+  if (v < min) {
+    return min;
+  } else if (v > max) {
+    return max;
+  } else {
+    return v;
+  }
+}
+
+function rand(min, max) {
+  if (min === undefined) min = 0;
+  if (max === undefined) max = 1;
+  return min + Math.random() * (max - min);
+}
+
+function createPlayer(container) {
+  GAME_STATE.playerX = GAME_WIDTH / 2;
+  GAME_STATE.playerY = GAME_HEIGHT - 50;
+  const player = document.createElement("img");
+  player.src = "img/player-blue-1.png";
+  player.className = "player";
+  container.appendChild(player);
+  setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
+}
+
+function updatePlayer(dt, container) {
+  if (GAME_STATE.leftPressed) {
+    GAME_STATE.playerX -= dt * PLAYER_MAX_SPEED;
+  }
+  if (GAME_STATE.rightPressed) {
+    GAME_STATE.playerX += dt * PLAYER_MAX_SPEED;
+  }
+
+  GAME_STATE.playerX = clamp(
+    GAME_STATE.playerX,
+    PLAYER_WIDTH,
+    GAME_WIDTH - PLAYER_WIDTH
+  );
+
+  if (GAME_STATE.spacePressed && GAME_STATE.playerCooldown <= 0) {
+    createLaser(container, GAME_STATE.playerX, GAME_STATE.playerY);
+    GAME_STATE.playerCooldown = LASER_COOLDOWN;
+  }
+  if (GAME_STATE.playerCooldown > 0) {
+    GAME_STATE.playerCooldown -= dt;
+  }
+
+  const player = document.querySelector(".player");
+  setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
+}
+
+function createLaser(container, x, y) {
+  const element = document.createElement("img");
+  element.src = "img/laser-blue-1.png";
+  element.className = "laser";
+  container.appendChild(element);
+  const laser = { x, y, element };
+  GAME_STATE.lasers.push(laser);
+  const audio = new Audio("sound/sfx-laser1.ogg");
+  audio.play();
+  setPosition(element, x, y);
+}
+
+function updateLasers(dt, container) {
+  const lasers = GAME_STATE.lasers;
+  for (let i = 0; i < lasers.length; i++) {
+    const laser = lasers[i];
+    laser.y -= dt * LASER_MAX_SPEED;
+    if (laser.y < 0) {
+      destroyLaser(container, laser);
+    }
+    setPosition(laser.element, laser.x, laser.y);
+    const r1 = laser.element.getBoundingClientRect();
+    const enemies = GAME_STATE.enemies;
+    for (let j = 0; j < enemies.length; j++) {
+      const enemy = enemies[j];
+      if (enemy.isDead) continue;
+      const r2 = enemy.element.getBoundingClientRect();
+      if (rectsIntersect(r1, r2)) {
+        // Enemy was hit
+        destroyEnemy(container, enemy);
+        destroyLaser(container, laser);
+        break;
+      }
+    }
+  }
+  GAME_STATE.lasers = GAME_STATE.lasers.filter(e => !e.isDead);
+}
+
+function destroyLaser(container, laser) {
+  container.removeChild(laser.element);
+  laser.isDead = true;
+}
+
+function createEnemy(container, x, y) {
+  const element = document.createElement("img");
+  element.src = "img/enemy-blue-1.png";
+  element.className = "enemy";
+  container.appendChild(element);
+  const enemy = {
+    x,
+    y,
+    cooldown: rand(0.5, ENEMY_COOLDOWN),
+    element
+  };
+  GAME_STATE.enemies.push(enemy);
+  setPosition(element, x, y);
+}
+
+function updateEnemies(dt, container) {
+  const dx = Math.sin(GAME_STATE.lastTime / 1000.0) * 50;
+  const dy = Math.cos(GAME_STATE.lastTime / 1000.0) * 10;
+
+  const enemies = GAME_STATE.enemies;
+  for (let i = 0; i < enemies.length; i++) {
+    const enemy = enemies[i];
+    const x = enemy.x + dx;
+    const y = enemy.y + dy;
+    setPosition(enemy.element, x, y);
+    enemy.cooldown -= dt;
+    if (enemy.cooldown <= 0) {
+      createEnemyLaser(container, x, y);
+      enemy.cooldown = ENEMY_COOLDOWN;
+    }
+  }
+  GAME_STATE.enemies = GAME_STATE.enemies.filter(e => !e.isDead);
+}
+
+function destroyEnemy(container, enemy) {
+  container.removeChild(enemy.element);
+  enemy.isDead = true;
+}
+
+function createEnemyLaser(container, x, y) {
+  const element = document.createElement("img");
+  element.src = "img/laser-red-5.png";
+  element.className = "enemy-laser";
+  container.appendChild(element);
+  const laser = { x, y, element };
+  GAME_STATE.enemyLasers.push(laser);
+  setPosition(element, x, y);
+}
+
+function updateEnemyLasers(dt, container) {
+  const lasers = GAME_STATE.enemyLasers;
+  for (let i = 0; i < lasers.length; i++) {
+    const laser = lasers[i];
+    laser.y += dt * LASER_MAX_SPEED;
+    if (laser.y > GAME_HEIGHT) {
+      destroyLaser(container, laser);
+    }
+    setPosition(laser.element, laser.x, laser.y);
+  }
+  GAME_STATE.enemyLasers = GAME_STATE.enemyLasers.filter(e => !e.isDead);
+}
+
+
+function init() {
+  const container = document.querySelector(".game");
+  createPlayer(container);
+
+  const enemySpacing = (GAME_WIDTH - ENEMY_HORIZONTAL_PADDING * 2) / (ENEMIES_PER_ROW - 1);
+  for (let j = 0; j < 3; j++) {
+    const y = ENEMY_VERTICAL_PADDING + j * ENEMY_VERTICAL_SPACING;
+    for (let i = 0; i < ENEMIES_PER_ROW; i++) {
+      const x = i * enemySpacing + ENEMY_HORIZONTAL_PADDING;
+      createEnemy(container, x, y);
+    }
+  }
+}
+
+function update(e) {
+  const currentTime = Date.now();
+  const dt = (currentTime - GAME_STATE.lastTime) / 1000.0;
+
+  const container = document.querySelector(".game");
+  updatePlayer(dt, container);
+  updateLasers(dt, container);
+  updateEnemies(dt, container);
+  updateEnemyLasers(dt, container);
+
+  GAME_STATE.lastTime = currentTime;
+  window.requestAnimationFrame(update);
+}
+
+function onKeyDown(e) {
+  if (e.keyCode === KEY_CODE_LEFT) {
+    GAME_STATE.leftPressed = true;
+  } else if (e.keyCode === KEY_CODE_RIGHT) {
+    GAME_STATE.rightPressed = true;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = true;
+  }
+}
+
+function onKeyUp(e) {
+  if (e.keyCode === KEY_CODE_LEFT) {
+    GAME_STATE.leftPressed = false;
+  } else if (e.keyCode === KEY_CODE_RIGHT) {
+    GAME_STATE.rightPressed = false;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = false;
+  }
+}
+
+init();
+window.addEventListener("keydown", onKeyDown);
+window.addEventListener("keyup", onKeyUp);
+window.requestAnimationFrame(update);
